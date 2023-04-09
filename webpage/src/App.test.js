caf1ced699ea133c82aa3066d47a28438fb69f70
Dakshin Karthikeyan <dakshinamoorthyk@sahaj.ai>
2023-04-09 19:32:33 +0530

    :tada: Second code change!

--- webpage/src/App.test.js
+++ webpage/src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders 🐱', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const cat = screen.getByText(/🐱/);
+  
+  expect(cat).toBeInTheDocument();
 });
