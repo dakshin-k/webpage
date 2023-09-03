33ac3820d17ca1edeac6d387001fbcdf0ce331a3
Dakshin Karthikeyan <dakshinamoorthyk@sahaj.ai>
2023-09-03 13:09:38 +0530

    hmmm

--- webpage/src/App.js
+++ webpage/src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar>
+        <Container>
+          <Navbar.Brand>Dakshin K</Navbar.Brand>
+          <Navbar.Toggle>Blog</Navbar.Toggle>
+          <Navbar.Toggle>Projects</Navbar.Toggle>
+          <Navbar.Toggle>Talks</Navbar.Toggle>
+        </Container>
+      </Navbar>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           🐱
         </p>
